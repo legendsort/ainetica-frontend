041fa5064643ccf3294e094554c40055c8b98a42
Sortoite <zachatatem@gmail.com>
2023-04-03 22:43:07 -0400

    Feat: half of footer

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,15 +1,26 @@
-import type { FC } from "react";
-
-const Footer: FC = () => {
+const Footer = () => {
   return (
-    <div className="flex items-center justify-center">
-      <div
-        className="inline-block h-8 w-8 animate-[spinner-grow_0.75s_linear_infinite] rounded-full bg-current align-[-0.125em] opacity-0 motion-reduce:animate-[spinner-grow_1.5s_linear_infinite]"
-        role="status"
-      >
-        <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
-          Loading...
-        </span>
+    <div className="bg-[#D9D9D9] min-h-[322px] text-black">
+      <div className="container grid grid-cols-2 mx-auto">
+        <div className="col-span-2 flex items-center justify-center p-3 md:col-span-1 pt-20">
+          <div className="flex-shrink-0">
+            <img src="/assets/logo/logo-black.png" alt="Ainetica" />
+          </div>
+        </div>
+        {/* <div className="col-span-2 flex items-center justify-center p-3 md:col-span-1 pt-20">
+          <div className="gird-cols-3 mx-auto">
+            <div className="col-span-1 flex items-center justify-center">
+                asdf
+            </div>
+            <div className="col-span-1 flex items-center justify-center">
+                asdf
+            </div>
+            <div className="col-span-1 flex items-center justify-center">
+                asdf
+            </div>
+          </div>
+        </div> */}
+        
       </div>
     </div>
   );
